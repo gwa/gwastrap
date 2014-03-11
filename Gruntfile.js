298def4f81f02ec43651716f010d9a82ebc1c4eb
Timothy Groves <groves@greatwhiteark.com>
2014-03-11 06:58:39 +0100

    CSS and HTML validation

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,21 @@ module.exports = function(grunt) {
 		// tasks
 		uglify: grunt.file.readJSON('./grunt/tasks/uglify.json'),
 		jshint: grunt.file.readJSON('./grunt/tasks/jshint.json'),
-		csslint: grunt.file.readJSON('./grunt/tasks/csslint.json')
+		csslint: grunt.file.readJSON('./grunt/tasks/csslint.json'),
+		validation: grunt.file.readJSON('./grunt/tasks/validation.json')
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-html-validation');
 
 	grunt.registerTask(
 		'default',
 		[
 			'csslint:src',
 			'jshint:src',
+			'validation:src',
 			'uglify'
 		]
 	);
